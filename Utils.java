d227cb1ec40caee7acc25c7565b703da62e98b83
wwf <wwf>
2018-01-22 16:24:22 +0800

    添加了, android软键盘监听类

---
+++ Utils.java
@@ -0,0 +1,48 @@
+package com.test.zysnail.www.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.Resources;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+
+import static com.example.woniu.zysnail_common.utils.CommonUtil.isNavigationBarShow;
+import static com.umeng.socialize.utils.DeviceConfig.context;
+
+/**
+ * Created by zy01060 on 2018/1/22.
+ */
+
+public class Utils {
+    public static void showKeyBoard(Context context, View view) {
+        InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.showSoftInput(view,InputMethodManager.SHOW_FORCED);
+    }
+    public static void hideKeyBoard(View view) {
+        InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0); //强制隐藏键盘
+    }
+    //获取状态栏的高度
+    public static int getStatusHeight(Context context) {
+        int statusBarHeight = -1;
+        //获取status_bar_height资源的ID
+        int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            //根据资源ID获取响应的尺寸值
+            statusBarHeight = context.getResources().getDimensionPixelSize(resourceId);
+        }
+        return statusBarHeight;
+    }
+    //获取底部虚拟按键的高度
+    public static int getNavigationBarHeight(Activity activity) {
+        if (!isNavigationBarShow(activity)){
+            return 0;
+        }
+        Resources resources = activity.getResources();
+        int resourceId = resources.getIdentifier("navigation_bar_height",
+                "dimen", "android");
+        //获取NavigationBar的高度
+        int height = resources.getDimensionPixelSize(resourceId);
+        return height;
+    }
+}
